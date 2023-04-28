08a32c960209a9861c4a495aa9a258deb07797ba
paytonshafer <paytonshafer@gmail.com>
2023-04-28 13:31:04 -0400

    finish sha3library uses

--- SHA-3/sha3libraries.py
+++ SHA-3/sha3libraries.py
@@ -52,89 +52,26 @@ with open('sha3.pdf', 'rb') as f:
     digest = hashlib.file_digest(f, "sha3_256")
 print('256: ' + digest.hexdigest())
 
-print('\nHASHLIB-SHAKE: data  = ' + data + '\n')
+print('\nHASHLIB-SHAKE: data = ' + data + '\n')
 
 #also has all shake variations
 n = hashlib.shake_256()
 
 n.update(data.encode())
 #different length outputs
+shake = input('Please enter the amount of bytes you would like: ')
+print(shake + ' byte (' + str(int(shake)*8) +' bit) output: ' + n.hexdigest(int(shake)))
 print('32 byte (256 bit) output: ' + n.hexdigest(32))
 print('43 byte (344 bit) output: ' + n.hexdigest(43))
 
-K12 = KangarooTwelve.new(custom=b'stringforcustomization') #the custom is so that 2 hashes with different custom str but same data will have different digest
-K12.update(b'Some data')
-print('K12 w str1:', K12.read(26).hex()) #read takes how man bytes as output
+print('\nCRYPTO.HASH-K12: data = ' + data + '\n')
+[key1, key2] = input('Please enter two different keys seperated by a space: ').split(' ')
+length = int(input('Please enter the number of bytes of output you\'d like: '))
 
-K12 = KangarooTwelve.new(custom=b'otherstringfork12') #the custom is so that 2 hashes with different custom str but same data will have different digest
-K12.update(b'Some data')
-print('K12 w str2:', K12.read(26).hex()) #read takes how man bytes as output
+K12 = KangarooTwelve.new(custom=key1.encode()) #the custom is so that 2 hashes with different custom str but same data will have different digest
+K12.update(data.encode())
+print('K12 key = ' + key1 + ': ' +  K12.read(length).hex()) #read takes how man bytes as output
 
-
-'''
-from Crypto.Hash import SHA3_224, SHA3_256, SHA3_384, SHA3_512
-h_obj = SHA3_224.new()
-h_obj.update(b'hello world')
-print('SHA3_224:', h_obj.hexdigest())
-
-h_obj = SHA3_256.new()
-h_obj.update(b'hello world')
-print('SHA3_256:',h_obj.hexdigest())
-
-
-NOTE Below is all for Crypto.Hash
-.new() can take a data= argument for data
-
-hash_object.update(b'Second')
-hash_object.update(b'Third')
-The two steps above are equivalent to:
-hash_object.update(b'SecondThird')
-'''
-'''
-#SHA-3 Family
-from Crypto.Hash import SHA3_224, SHA3_256, SHA3_384, SHA3_512
-h_obj = SHA3_224.new()
-h_obj.update(b'Some data')
-print('SHA3_224:', h_obj.hexdigest())
-
-h_obj = SHA3_256.new()
-h_obj.update(b'Some data')
-print('SHA3_245:',h_obj.hexdigest())
-
-h_obj = SHA3_384.new()
-h_obj.update(b'Some data')
-print('SHA3_384:',h_obj.hexdigest())
-
-h_obj = SHA3_512.new()
-h_obj.update(b'Some data')
-print('SHA3_512:',h_obj.hexdigest())
-
-#XOF(extendable output function) Shake/cShake
-from Crypto.Hash import SHAKE128, SHAKE256, KangarooTwelve
-
-shake = SHAKE128.new()
-shake.update(b'Some data')
-print('SHAKE128:', shake.read(26).hex()) #read takes how man bytes as output
-
-shake = SHAKE256.new()
-shake.update(b'Some data')
-print('SHAKE256:', shake.read(26).hex()) #read takes how man bytes as output
-
-K12 = KangarooTwelve.new(custom=b'stringforcustomization') #the custom is so that 2 hashes with different custom str but same data will have different digest
-K12.update(b'Some data')
-print('K12 w str1:', K12.read(26).hex()) #read takes how man bytes as output
-
-K12 = KangarooTwelve.new(custom=b'otherstringfork12') #the custom is so that 2 hashes with different custom str but same data will have different digest
-K12.update(b'Some data')
-print('K12 w str2:', K12.read(26).hex()) #read takes how man bytes as output
-
-#there is also use in message passing
-from Crypto.Hash import KMAC128, KMAC256 #128 has 16 byte key, 256 has 32 byte key
-
-secret = b'Sixteen byte key'
-mac = KMAC128.new(key=secret, mac_len=16) #mac_len is an extra parameter for output
-mac.update(b'Hello')
-print('KMAC128:', mac.hexdigest())
-
-#cant figure out what else to do
-'''
+K12 = KangarooTwelve.new(custom=key2.encode()) #the custom is so that 2 hashes with different custom str but same data will have different digest
+K12.update(data.encode())
+print('K12 key = ' + key2 + ': ' + K12.read(length).hex()) #read takes how man bytes as output
